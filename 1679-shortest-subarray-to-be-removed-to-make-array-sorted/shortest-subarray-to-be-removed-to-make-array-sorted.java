2a424d17dfb3bcb7d959f0fa153dd5e1a9459f4e
Aman Kumar <120359477+amankr212003@users.noreply.github.com>
2026-09-13 02:32:27 +0530

    Time: 4 ms (8.17%) | Memory: 92.4 MB (94.16%) - LeetSync

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.java
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.java
@@ -1,25 +1,39 @@
 class Solution {
-    public int findLengthOfShortestSubarray(int[] arr) {
-        int l=0;
-        int n = arr.length;
-        int r=n-1;
 
-        while(r>0 && arr[r]>=arr[r-1])
-            r--;
-        
-        int shortest=r;// Case-1: Remove all elements to the left of r
-
-        while(l<r){
-            while(r<n && arr[l]>arr[r])
-                r++;
-            shortest=Math.min(r-l-1,shortest);
-             // r-l-1 means removing all elements between l and r
-
-            l++;
-
-            if(arr[l]<arr[l-1])
-                break;
+    private int helperBinarySearch(int[] arr, int left, int right, int target) {
+        // Find the first index where arr[mid] >= target
+        // finding lowerbound
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] >= target) right = mid;
+            else left = mid + 1;
         }
-        return shortest;
+        return left;
+    }
+
+    public int findLengthOfShortestSubarray(int[] arr) {
+        int n = arr.length;
+        int left = 0, right = n - 1;
+
+        // Find the longest non-decreasing subarray from the left
+        while (left + 1 < n && arr[left] <= arr[left + 1]) left++;
+
+        // Find the longest non-decreasing subarray from the right
+        while (right - 1 >= 0 && arr[right] >= arr[right - 1]) right--;
+
+        // If the entire array is already sorted
+        if (left >= right) return 0;
+
+        // Start with removing either left or right part completely
+        int ans = Math.min(n - (left + 1), right);
+
+        // Try to merge the left and right parts
+        for (int i = 0; i <= left; i++) {
+            int target = arr[i];
+            int j = helperBinarySearch(arr, right, n, target);
+            ans = Math.min(ans, j - (i + 1));
+        }
+
+        return ans;
     }
 }
