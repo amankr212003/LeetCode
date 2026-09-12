46c19f9138682cfa6a4c20e7045b5e6e12f33ec8
Aman Kumar <120359477+amankr212003@users.noreply.github.com>
2026-09-13 02:32:38 +0530

    Time: 1 ms (100.00%) | Memory: 92.4 MB (95.91%) - LeetSync

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.java
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.java
@@ -1,39 +1,25 @@
 class Solution {
-
-    private int helperBinarySearch(int[] arr, int left, int right, int target) {
-        // Find the first index where arr[mid] >= target
-        // finding lowerbound
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] >= target) right = mid;
-            else left = mid + 1;
-        }
-        return left;
-    }
-
     public int findLengthOfShortestSubarray(int[] arr) {
+        int l=0;
         int n = arr.length;
-        int left = 0, right = n - 1;
+        int r=n-1;
 
-        // Find the longest non-decreasing subarray from the left
-        while (left + 1 < n && arr[left] <= arr[left + 1]) left++;
+        while(r>0 && arr[r]>=arr[r-1])
+            r--;
+        
+        int shortest=r;// Case-1: Remove all elements to the left of r
 
-        // Find the longest non-decreasing subarray from the right
-        while (right - 1 >= 0 && arr[right] >= arr[right - 1]) right--;
+        while(l<r){
+            while(r<n && arr[l]>arr[r])
+                r++;
+            shortest=Math.min(r-l-1,shortest);
+             // r-l-1 means removing all elements between l and r
 
-        // If the entire array is already sorted
-        if (left >= right) return 0;
+            l++;
 
-        // Start with removing either left or right part completely
-        int ans = Math.min(n - (left + 1), right);
-
-        // Try to merge the left and right parts
-        for (int i = 0; i <= left; i++) {
-            int target = arr[i];
-            int j = helperBinarySearch(arr, right, n, target);
-            ans = Math.min(ans, j - (i + 1));
+            if(arr[l]<arr[l-1])
+                break;
         }
-
-        return ans;
+        return shortest;
     }
 }
